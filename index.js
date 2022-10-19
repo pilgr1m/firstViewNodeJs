7253782b77f649bf75dc23be46e556afdd2bc6f0
vvane <vanjavolokha@gmail.com>
2022-10-19 15:08:59 +0300

    files

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-const myMath = require('./myMath')
-const result = myMath.minus(5,5)
-const result2 = myMath.add(5,5)
+// const myMath = require('./myMath')
+// const result = myMath.minus(5,5)
+// const result2 = myMath.add(5,5)
+// console.log(result, result2)
 
-console.log(result, result2)
+const fs = require('fs')
+
+// const read = fs.readFileSync('other.txt', 'utf-8')
+// console.log(read)
+
+// create (if not exist) and write 
+// or REWRITE(if file is empty) into file
+// fs.writeFileSync('other.txt', read + '\n Hello world!!!')
+
+const read = fs.readFile('other.txt', 'utf-8', (error, data)=> {
+    console.log('data in read: ', data)
+    console.log('error in read: ', error)
+    fs.writeFile('other.txt', data + '\n new text test', (error, data)=> {
+        console.log('data in write: ', data)
+        console.log('error in write: ', error)
+    })
+    
+})
+console.log('READ: ', read)
+
+// fs.writeFile('other.txt', read + '\n Hello world!!!')
+
