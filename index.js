694db5ece4d558964cdf8de223f3c0d7abe816f4
vvane <vanjavolokha@gmail.com>
2022-10-19 15:36:01 +0300

    work with folders

--- index.js
+++ index.js
@@ -12,16 +12,32 @@ const fs = require('fs')
 // or REWRITE(if file is empty) into file
 // fs.writeFileSync('other.txt', read + '\n Hello world!!!')
 
-const read = fs.readFile('other.txt', 'utf-8', (error, data)=> {
-    console.log('data in read: ', data)
-    console.log('error in read: ', error)
-    fs.writeFile('other.txt', data + '\n new text test', (error, data)=> {
-        console.log('data in write: ', data)
-        console.log('error in write: ', error)
-    })
+// const read = fs.readFile('other.txt', 'utf-8', (error, data)=> {
+//     console.log('data in read: ', data)
+//     console.log('error in read: ', error)
+//     fs.writeFile('other.txt', data + '\n new text test', (error, data)=> {
+//         console.log('data in write: ', data)
+//         console.log('error in write: ', error)
+//     })
     
-})
-console.log('READ: ', read)
+// })
+// console.log('READ: ', read)
 
 // fs.writeFile('other.txt', read + '\n Hello world!!!')
 
+// fs.mkdirSync('new folder')
+
+// create new folder and then create new txt-file and write into
+// fs.mkdir('new-folder1', () => {
+//     fs.writeFile('./new-folder1/1file.txt', 'Test text', ()=>{})
+// })
+
+// unlink-> remove file,rmdir -> remove folder
+//  !!!folder have to empty and then we can remove it
+// fs.unlink('new-folder', () => { })
+// fs.unlink('./new-folder/1file.txt', () => {
+//     fs.rmdir('./new-folder', () => {})
+// })
+// fs.unlink('./wer/wer3.txt', () => {
+//     fs.rmdir('wer', () => {})
+// })
