00b64c1f858ad73855973ae3e3a4e7666ab136e6
vvane <vanjavolokha@gmail.com>
2022-10-19 16:02:45 +0300

    server and html

--- index.js
+++ index.js
@@ -50,7 +50,18 @@ let server = http.createServer((request, response) => {
     // console.log('response: ', response)
     
     response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
-    response.end('Hello world!!! <br> I study <b>Node.Js</b>')
+    response.end(`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>NODE.js</title>
+    </head>
+    <body>
+        Hello world!!! <br> <b>I study Node.Js</b>
+    </body>
+    </html>`)
 })
 
 
